55cc0b921b64d03e443bc2169ecf27d434925d11
khadijaz1 <khadujizamil@gmail.com>
2023-08-10 15:11:23 -0700

    2-assembler

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,10 +1,11 @@
 1 #include <stdio.h>
-2 /**
-3  * main - Entry point
-4 *
-5  * Return: Always 0 (Success
-6 */
-7 int main(void)
-8 {
-9    return (0);
-10 }
+^ ^
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    return (0);
+}
